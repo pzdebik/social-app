b0b0a85cbaf718bb3efa62395e14c7587e574bbd
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2024-01-28 09:25:09 +0100

    Create Angular client project

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,12 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
